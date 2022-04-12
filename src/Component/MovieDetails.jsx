46ffc45c4c80b43a4e1a6578e5f2b3b9a5607d02
farah2003 <farah.shaqoura@example.com>
2022-04-13 02:25:12 +0300

    details page

--- src/Component/MovieDetails.jsx
+++ src/Component/MovieDetails.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './css/MovieDetails.css';
 
-function MovieDetails() {
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState({});
+  const ImgApi = 'https://image.tmdb.org/t/p/w1280';
+  useEffect(() => {
+    const Details = movies.find(({ id }) => id === Number(movieId));
+    setMovieDetails(Details);
+  }, []);
+  console.log(movieDetails);
   return (
-    <div />
+
+    <div className="DetailsContainer">
+      <div>
+        <img className="imgDetails" src={ImgApi + movieDetails.poster_path} alt={movieDetails.title} />
+      </div>
+      <div className="Details">
+        <div className="detailsHeader">
+          <h2>
+            {movieDetails.title}
+          </h2>
+          <p>{movieDetails.vote_average}</p>
+        </div>
+        <button className="addBtn" type="submit" onClick={() => console.log('h')}>Add to my WatchList</button>
+
+        {/* backdrop_path */}
+        <p>{movieDetails.overview}</p>
+        <h4>
+
+          {'Release date:  '}
+
+          {movieDetails.release_date}
+        </h4>
+
+      </div>
+    </div>
   );
 }
+MovieDetails.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    overview: PropTypes.string,
+    vote_average: PropTypes.number.isRequired,
+  })),
+};
+MovieDetails.defaultProps = {
+  movies: [{}],
+};
 
 export default MovieDetails;
